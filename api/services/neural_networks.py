6e44a93bb9af57eba8d3954691d33361d42c1f2e
nyook941 <kennan.k.wu@gmail.com>
2024-09-12 23:47:18 -0500

    add different types of layer functionality

--- api/services/neural_networks.py
+++ api/services/neural_networks.py
@@ -4,27 +4,34 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import tensorflow as tf
 from tensorflow import keras
 
-def create_neural_network(layers, input_shape):
+def createNeuralNetwork(layers, inputShape):
     model = keras.Sequential()
 
     for i, layer in enumerate(layers):
-        if i == 0:
-            model.add(keras.layers.Dense(units=layer['units'], activation=layer['activation'], input_shape=input_shape))
+        layerType = getattr(keras.layers, layer['type'])
+        
+        if layer['type'] == 'Embedding':
+            model.add(layerType(input_dim=layer.get('inputDim', 1000),
+                                output_dim=layer.get('outputDim', 64),
+                                input_length=inputShape[0]))
         else:
-            model.add(keras.layers.Dense(units=layer['units'], activation=layer['activation']))
+            if i == 0:
+                model.add(layerType(units=layer['neurons'], activation=layer['activation'], input_shape=inputShape))
+            else:
+                model.add(layerType(units=layer['neurons'], activation=layer['activation']))
 
     model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-    model.build(input_shape=(None,) + input_shape)
-
+    
     return model
 
 layers = [
-    {'units': 64, 'activation': 'relu'},
-    {'units': 32, 'activation': 'relu'},
-    {'units': 10, 'activation': 'softmax'}
+    {'type': 'Embedding', 'inputDim': 1000, 'outputDim': 64}, 
+    {'type': 'Dense', 'neurons': 32, 'activation': 'relu'},
+    {'type': 'Dense', 'neurons': 10, 'activation': 'softmax'}
 ]
-input_shape = (100,)
 
-model = create_neural_network(layers, input_shape)
+inputShape = (100,)
+
+model = createNeuralNetwork(layers, inputShape)
 
 model.summary()
--- api/services/serializers.py
+++ api/services/serializers.py
@@ -1,8 +1,31 @@
 from rest_framework import serializers
+from tensorflow import keras
+from keras import layers
 
 class LayerSerializer(serializers.Serializer):
+    type = serializers.CharField(default="")
     neurons = serializers.IntegerField()
     activation = serializers.ChoiceField(choices=['relu', 'sigmoid', 'softmax', 'tanh'])
+    inputDim = serializers.IntegerField()
+    outputDim = serializers.IntegerField()
+
+    def validate_type(self, value):
+        try:
+            layer_class = eval(f"layers.{value}")
+            
+            if not isinstance(layer_class, type) or not issubclass(layer_class, layers.Layer):
+                raise serializers.ValidationError(f"{value} is not a valid Keras layer type.")
+        except (AttributeError, NameError):
+            raise serializers.ValidationError(f"{value} is not a valid Keras layer type.")
+        
+        return value
+
 
 class NeuralNetworkSerializer(serializers.Serializer):
     layers = LayerSerializer(many=True)
+
+    def validateLength(self, data):
+        layers = data.get('layers', [])
+        if len(layers) < 2:
+            message = "The neural network must have more than a single layer"
+            raise serializers.ValidationError(message)

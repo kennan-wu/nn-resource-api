edd41a7ada7e4b0cb2697484c463b1f14ce00ec2
nyook941 <73042201+nyook941@users.noreply.github.com>
2024-12-16 12:07:18 -0600

    remove test services

--- api/services/aws_services/S3_file_manager.py
+++ api/services/aws_services/S3_file_manager.py
@@ -6,8 +6,6 @@ from io import BytesIO
 
 class S3FileManager:
     def __init__(self, bucket_name, aws_access_key=None, aws_secret_key=None, region_name=None):
-        print(f"AWS Access Key: {aws_access_key}")
-        aws_access_key
         self.s3_client = boto3.client(
             's3',
             aws_access_key_id=aws_access_key,
@@ -15,7 +13,6 @@ class S3FileManager:
             region_name=region_name
         )
         self.bucket_name = bucket_name
-
 
     def upload_stream(self, file_stream, s3_key):
         """
@@ -32,6 +29,11 @@ class S3FileManager:
             print(f"Failed to upload file: {e}")
 
     def download_file(self, s3_key, local_path):
+        """
+        Downloads a file from the specified S3 bucket.
+        :param s3_key: The key (path/filename) in S3 to download.
+        :param local_path: The local path where the file should be saved.
+        """
         try:
             self.s3_client.download_file(self.bucket_name, s3_key, local_path)
             print(f"File '{s3_key}' downloaded successfully from '{self.bucket_name}' to '{local_path}'.")
@@ -39,5 +41,3 @@ class S3FileManager:
             print("Credentials not available.")
         except ClientError as e:
             print(f"Failed to download file: {e}")
-            raise e  # Re-raise the error to capture details in the API response
-
--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),  # Index route
-    path("create/", views.NeuralNetworkViewSet.as_view({'post': 'create'}), name='create_neural_network'),  # Create route
-    path("test-download/", views.NeuralNetworkViewSet.as_view({'get': 'test_download'}), name='test_download'),  # Test download route
+    path("", views.index, name="index"),
+    path("create", views.NeuralNetworkViewSet.as_view({'post': 'create'}), name='create_neural_network')
 ]

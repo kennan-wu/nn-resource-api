26da98b37e201ef347a5857e3a750f3af4da82bd
nyook941 <73042201+nyook941@users.noreply.github.com>
2025-02-06 20:57:24 -0600

    add delete endpoint

--- routes/nn_routes.py
+++ routes/nn_routes.py
@@ -6,18 +6,19 @@ from models.request.create_nn_request import CreateNNRequest
 
 nn_router = APIRouter(prefix="/user/{user_id}/nn")
 
+
 @nn_router.get("/")
 async def get_all_neural_networks(
-    user_id: str, 
-    nn_service: nn_service_dep,
-    user_service: user_service_dep    
+    user_id: str, nn_service: nn_service_dep, user_service: user_service_dep
 ):
     user = user_service.get_user(user_id)
     return nn_service.getAllNN(user)
 
+
 @nn_router.get("/{nn_id}")
 async def get_nn_with_id(nn_id: str, nn_service: nn_service_dep):
     return nn_service.getNN(nn_id)
+
 
 @nn_router.post("/")
 async def create_neural_network(
@@ -26,7 +27,19 @@ async def create_neural_network(
     nn_service: nn_service_dep,
     user_service: user_service_dep,
     keras_service: keras_service_dep,
-    s3_service: s3_service_dep
+    s3_service: s3_service_dep,
 ):
     user = user_service.get_user(user_id)
     return nn_service.createNN(user, nn_data, user_service, keras_service, s3_service)
+
+
+@nn_router.delete("/delete/{nn_id}")
+async def delete_neural_network(
+    nn_id: str,
+    user_id: str,
+    nn_service: nn_service_dep,
+    s3_service: s3_service_dep,
+    user_service: user_service_dep,
+):
+    removed_id = nn_service.deleteNN(nn_id, user_id, user_service, s3_service)
+    return removed_id
